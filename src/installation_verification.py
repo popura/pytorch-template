846de93bb662abfd21969d9548b97176ec935829
popura <popura.net@gmail.com>
2022-05-24 13:15:03 +0900

    some fixed

--- src/installation_verification.py
+++ src/installation_verification.py
@@ -1,10 +1,10 @@
 import torch
 
 print(torch.__version__)
-print(f"cuda, {torch.cuda.is_available()}")
-print(f"compute_{''.join(map(str,(torch.cuda.get_device_capability())))}")
+print(f"Is CUDA available?: {torch.cuda.is_available()}")
+print(f"CUDA compute capability: {''.join(map(str,(torch.cuda.get_device_capability())))}")
 device_num:int = torch.cuda.device_count()
-print(f"find gpu devices, {device_num}")
+print(f"# of GPU devices: {device_num}")
 for idx in range(device_num):
     print(f"cuda:{idx}, {torch.cuda.get_device_name(idx)}")
 
