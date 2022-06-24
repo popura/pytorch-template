18808f20d40196c1084f79ed54e3e2c726d66760
Yuma Kinoshita <ykinoshita@tmu.ac.jp>
2022-06-24 11:21:15 +0900

    added verification script

---
+++ src/installation_verification.py
@@ -0,0 +1,11 @@
+import torch
+
+print(torch.__version__)
+print(f"Is CUDA available?: {torch.cuda.is_available()}")
+print(f"CUDA compute capability: {''.join(map(str,(torch.cuda.get_device_capability())))}")
+device_num:int = torch.cuda.device_count()
+print(f"# of GPU devices: {device_num}")
+for idx in range(device_num):
+    print(f"cuda:{idx}, {torch.cuda.get_device_name(idx)}")
+
+print("end")
